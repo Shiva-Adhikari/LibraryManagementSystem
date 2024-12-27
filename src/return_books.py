eda850cb5a6e83fb556523fcf81cbda9a39816b8
Shiva Adhikari <s2whl6xh@duck.com>
2024-12-27 15:33:15 +0545

    fixed with id

--- src/return_books.py
+++ src/return_books.py
@@ -1,16 +1,13 @@
-from issue_books import process_book 
+from .issue_books import process_book 
 import click
 
 
 @click.command()
-@click.option('--categories_check', prompt=('What type of Books you need'), type=str)
+@click.option('--categories_check', prompt=('Enter Book Category'), type=str)
 @click.option('--input_book_id', prompt=('Enter Book Id'), type=int)
 def return_books(categories_check, input_book_id):
-    process_book(categories_check, input_book_id, 0, 1)
+    process_book(categories_check, input_book_id, 0, 1)     # available = 1     # set-available = 0
 
 
 def main():
     return_books()
-
-if __name__ == '__main__':
-    main()
