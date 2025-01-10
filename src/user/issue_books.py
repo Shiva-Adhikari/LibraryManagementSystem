aef0a56a420f4f6be44e2cf12c5d56777bc5abc8
Shiva Adhikari <s2whl6xh@duck.com>
2025-01-11 00:40:11 +0545

    user can successfully issue_books

--- src/user/issue_books.py
+++ src/user/issue_books.py
@@ -1,46 +1,39 @@
 import click
+import json
 from pymongo import MongoClient
 from datetime import datetime
+
+from config import data_path
 
 
 client = MongoClient('localhost', 27017)
 db = client.LibraryManagementSystem
 
 
-def process_book(input_categories, input_book_name, set_available, to_date):
+@click.command()
+@click.option('--input-categories', prompt=('Enter Book Category'), type=str)
+@click.option('--input-book-name', prompt=('Enter Book Name'), type=str)
+@click.option('--to-date', prompt='For how many days you need', type=int)
+def issue_books(input_categories: str, input_book_name: str, to_date: int,
+                set_available=0) -> None:
     input_categories = input_categories.lower()
+    user_detail = {}
+    user_detail = data_path()
+    with open(user_detail) as file:
+        user_detail = json.load(file)
     result = db.Books.update_one(
         {f'{input_categories}.Title': input_book_name},
         {
             '$set': {
                 f'{input_categories}.$.Available': set_available,
                 f'{input_categories}.$.Days': to_date,
-                f'{input_categories}.$.Date': datetime.now()
+                f'{input_categories}.$.Date': datetime.now(),
+                f'{input_categories}.$.Details': user_detail
             }
         }
     )
     if result.modified_count > 0:
-        print('You got books')
-
-
-@click.command()
-@click.option('--input-categories', prompt=('Enter Book Category'), type=str)
-@click.option('--input-book-name', prompt=('Enter Book Name'), type=str)
-@click.option('--to-date', prompt='For how many days you need', type=int)
-def _issue_books(input_categories: str, input_book_name: str, to_date: int) -> None:
-    process_book(
-        input_categories,
-        input_book_name,
-        0,   # set-available = 0 for issue books
-        to_date
-        )
-
-
-def main():
-    _issue_books()
-
-
-if __name__ == '__main__':
-    main()
-
-"""Fix issue books also add user email to database when update"""
+        click.echo('You got book')
+    else:
+        """ ADD LOGGING MODULE """
+        click.echo('Unable to get book')
