e9619e274452f508ab1fba6d5207efe7bc046ef5
Shiva Adhikari <s2whl6xh@duck.com>
2025-01-23 16:36:52 +0545

    user can return book

--- src/user/return_books.py
+++ src/user/return_books.py
@@ -2,6 +2,7 @@ import click
 from pymongo import MongoClient
 
 from config import logging_module
+from config import verify_jwt_token
 
 
 logger = logging_module()
@@ -11,27 +12,42 @@ db = client.LibraryManagementSystem
 
 
 @click.command()
-@click.option('--input-categories', prompt=('Enter Book Category'), type=str)
-@click.option('--input-book-name', prompt=('Enter Book Name'), type=str)
-def return_books(input_categories: str, input_book_name: str, set_available=1
-                 ) -> None:
-    input_categories = input_categories.lower()
+@click.option(
+    '--input-categories',
+    prompt=('Enter Book Category'.lower()),
+    type=str
+)
+@click.option(
+    '--input-book-name',
+    prompt=('Enter Book Name'.lower()),
+    type=str
+)
+def return_books(input_categories: str, input_book_name: str) -> None:
+    user_details = verify_jwt_token()
+    username = user_details['username']
+    email = user_details['email']
+    # fetch data or remove data like this code
+    # don't use other method to remove this type of nested data.
     result = db.Books.update_one(
-        {f'{input_categories}.Title': input_book_name},
         {
-            '$set': {
-                f'{input_categories}.$.Available': set_available
+            f'{input_categories}.Title': input_book_name,
+            f'{input_categories}.UserDetails.Username': username,
+            f'{input_categories}.UserDetails.Email': email,
+        }, {
+            # remove data from nested database
+            '$pull': {
+                f'{input_categories}.$.UserDetails': {
+                    'Username': username,
+                    'Email': email
+                }
             },
-            '$unset': {
-                f'{input_categories}.$.IssueDate': '',
-                f'{input_categories}.$.Days': '',
-                f'{input_categories}.$.DueWarning': '',
-                f'{input_categories}.$.DueDate': '',
-                f'{input_categories}.$.Details': ''
+            # increase Available book by 1
+            '$inc': {
+                f'{input_categories}.$.Available': 1
             }
-        },
-        upsert=False
+        }
     )
+
     if result.modified_count > 0:
         click.echo('You return books')
     else:
