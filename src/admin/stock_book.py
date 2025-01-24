8624dcfdd779af1cd6d2b61eb3b49c8ab4f77984
Shiva Adhikari <s2whl6xh@duck.com>
2025-01-24 09:43:35 +0545

    check book stock

---
+++ src/admin/stock_book.py
@@ -0,0 +1,47 @@
+import click
+from tabulate import tabulate
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.LibraryManagementSystem
+
+
+def find():
+    results = db.Books.find()
+    for result in results:
+        category_keys = next(iter(result.keys() - {'_id': 0}))
+        print(f'keys:{category_keys}')
+    return category_keys
+
+
+def stock_book():
+    category = find()
+    results = db.Books.aggregate([
+        {'$unwind': f'${category}'},
+        {
+            '$match': {
+                f'{category}.Available': {'$lt': 5}
+            }
+        }, {
+            '$project': {
+                'Title': f'${category}.Title',
+                'Available': f'${category}.Available',
+                '_id': 0
+            }
+        }
+    ])
+
+    table = []
+    for result in results:
+        header = ['Category', 'Title', 'Available']
+        table.append([
+            category.capitalize(),
+            result['Title'].capitalize(),
+            result['Available']
+        ])
+
+    click.echo(tabulate(table, headers=header, tablefmt='mixed_grid'))
+
+
+if __name__ == '__main__':
+    stock_book()
