e550ed4140d48f3e0e14776b9a8534a0ffac9335
Shiva Adhikari <s2whl6xh@duck.com>
2025-01-25 16:33:30 +0545

    email can be sent from mutliple category books

--- src/schedule_book_check.py
+++ src/schedule_book_check.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 from pymongo import MongoClient
 from email.message import EmailMessage
 from dotenv import load_dotenv
+from src.admin.stock_book import find_keys
 
 
 client = MongoClient('localhost', 27017)
@@ -16,41 +17,46 @@ db = client.LibraryManagementSystem
 
 
 def due_book_check():
+    category_key = find_keys()
     today_date_ = datetime.now()
     today_date = datetime.date(today_date_)
-    fetch_data = db.Books.aggregate([
-        {'$unwind': '$bca'},
-        {'$unwind': '$bca.UserDetails'},
-        {
-            '$match': {
-                    'bca.UserDetails.IssueDate': {'$lte': today_date_}
+    for category in category_key:
+        fetch_data = db.Books.aggregate([
+            {'$unwind': f'${category}'},
+            {'$unwind': f'${category}.UserDetails'},
+            {
+                '$match': {
+                        f'{category}.UserDetails.IssueDate': {
+                            '$lte': today_date_
+                        }
+                }
+            },
+            {
+                '$project': {
+                    'Username': f'${category}.UserDetails.Username',
+                    'Email': f'${category}.UserDetails.Email',
+                    'DueWarning': f'${category}.UserDetails.IssueDate',
+                    'DueDate': f'${category}.UserDetails.DueDate'
+                }
             }
-        },
-        {
-            '$project': {
-                'Username': '$bca.UserDetails.Username',
-                'Email': '$bca.UserDetails.Email',
-                'DueWarning': '$bca.UserDetails.DueWarning',
-                'DueDate': '$bca.UserDetails.DueDate'
-            }
-        }
-    ])
-    for book in fetch_data:
-        due_warning = book['DueWarning']
-        due_warning_day = datetime.date(due_warning)
-        due_date = book['DueDate']
-        due_date_day = datetime.date(due_date)
+        ])
+        for book in fetch_data:
+            print(f'category: {category}')
+            due_warning = book['DueWarning']
+            due_warning_day = datetime.date(due_warning)
+            due_date = book['DueDate']
+            due_date_day = datetime.date(due_date)
 
-        if today_date == due_warning_day:
-            user_username = book['Username']
-            user_email = book['Email']
-            due_warning_text = 'DueWarning'
-            send_email(user_username, user_email, due_warning_text)
-        if today_date == due_date_day:
-            user_username = book['Username']
-            user_email = book['Email']
-            due_date_text = 'DueDate'
-            send_email(user_username, user_email, due_date_text)
+            if today_date == due_warning_day:
+                user_username = book['Username']
+                user_email = book['Email']
+                due_warning_text = 'DueWarning'
+                send_email(user_username, user_email, due_warning_text)
+            if today_date == due_date_day:
+                user_username = book['Username']
+                user_email = book['Email']
+                due_date_text = 'DueDate'
+                send_email(user_username, user_email, due_date_text)
 
 
 def send_email(user_username, user_email, due_text):
