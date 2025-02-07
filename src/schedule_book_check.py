5569440533ed0ab4ba2898a826cda92d1b489ce8
Shiva Adhikari <s2whl6xh@duck.com>
2025-02-07 22:11:47 +0545

    replace schedule to apscheduler module

--- src/schedule_book_check.py
+++ src/schedule_book_check.py
@@ -1,15 +1,13 @@
 import os
 import ssl
-import time
-import click
 import smtplib
-import schedule
 from datetime import datetime
-from datetime import timedelta
-from pymongo import MongoClient
 from email.message import EmailMessage
-from dotenv import load_dotenv
 from src.admin.stock_book import find_keys
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 client = MongoClient('localhost', 27017)
@@ -41,7 +39,6 @@ def due_book_check():
             }
         ])
         for book in fetch_data:
-            print(f'category: {category}')
             due_warning = book['DueWarning']
             due_warning_day = datetime.date(due_warning)
             due_date = book['DueDate']
@@ -98,23 +95,11 @@ LIBRARY"""
             smtp.login(email_sender, email_sender_password)
             smtp.sendmail(email_sender, email_receiver, em.as_string())
     except smtplib.SMTPRecipientsRefused:
-        click.echo('Email not Valid')
-
-
-def sleep_time():
-    now = datetime.now()
-    next_time = now.replace(hour=0, minute=1, second=0, microsecond=0)
-
-    if now > next_time:
-        next_time += timedelta(days=1)
-
-    return (next_time - now).seconds
+        # click.echo('Email not Valid')
+        pass
 
 
 if __name__ == '__main__':
-    while True:
-        s_t = sleep_time()
-        time.sleep(s_t)
-        schedule.every().day.at('00:1').do(due_book_check)
-        time.sleep(5)
-        schedule.run_pending()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(due_book_check, 'cron', hour=0)   # run every midnight
+    scheduler.start()
