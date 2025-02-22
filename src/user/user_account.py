ae09f276f4a6a2b1b15979a322bdb69b61dd2c02
Shiva Adhikari <s2whl6xh@duck.com>
2025-02-22 17:28:39 +0545

    removed unused code and added docs

--- src/user/user_account.py
+++ src/user/user_account.py
@@ -1,10 +1,5 @@
 import os
-import jwt
-import click
-from datetime import datetime
-from datetime import timedelta
 from dotenv import load_dotenv
-from email_validator import validate_email, EmailNotValidError
 
 from config import logging_module
 from src.admin.admin_account import account_login
@@ -18,49 +13,21 @@ env_path = os.path.join(src_path, '.env')
 load_dotenv(env_path)
 
 
-def email_validation() -> str:
-    while True:
-        try:
-            email = click.prompt('Enter Email', type=str)
-            email = validate_email(email, check_deliverability=False)
-            email = email.normalized
-            return email
-        except EmailNotValidError as e:
-            click.echo(str(e))
-            continue
-
-
-def validate_token(extract_username_email):
-    username = extract_username_email['username']
-    email = extract_username_email['email']
-    SECRET_KEY = os.getenv('jwt_user_secret')
-    ALGORITHM = 'HS256'
-    EXP_DATE = timedelta(minutes=5)
-    payload = {
-        'username': username,
-        'email': email,
-        'iat': int(datetime.now().timestamp()),
-        'exp': int((datetime.now() + EXP_DATE).timestamp())
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
-    return token
-
-
 def user_register():
+    """This is where User Register.
+    """
     whoami = 'User'
     account_register(whoami)
 
 
 def user_login():
+    """This is where User Login
+
+    Returns:
+        bool: if successfully login then it return True.
+    """
     whoami = 'User'
     access_token = 'SECRET_ACCESS_TOKEN_USER'
-    # username = 'user'
-    # password = 'user@123A'
     success_login = account_login(whoami, access_token)
-    # success_login = account_login(whoami, access_token, username, password)
     if success_login:
         return True
-
-
-if __name__ == '__main__':
-    user_login()
