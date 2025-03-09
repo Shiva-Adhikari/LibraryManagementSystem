a8a77c56e62526a0c79e751bbbc2010fd7f928bf
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-09 13:30:18 +0545

    imported module

---
+++ src/admin/__init__.py
@@ -0,0 +1,17 @@
+from src.admin.add_books import add_books
+from src.admin.stock_book import stock_book
+from src.admin.search_books import search_books
+from src.admin.update_books import update_books
+from src.admin.delete_books import delete_books
+from src.admin.admin_account import admin_login
+from src.admin.admin_account import admin_register
+
+__all__ = [
+    'add_books',
+    'stock_book',
+    'search_books',
+    'update_books',
+    'delete_books',
+    'admin_login',
+    'admin_register'
+]
