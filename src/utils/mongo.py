98835a78b4bdaa066d72e21313e44f7055723ac6
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-13 23:21:34 +0545

    added find_books and delete_books function

--- src/utils/mongo.py
+++ src/utils/mongo.py
@@ -19,8 +19,34 @@ def insert_books(department_name: str, new_books: list[int | str]):
         books = Books.objects(**{is_exists: True}).update_one(
             **{f'push_all__{department_name}': new_books},
             upsert=True
-        )
-        return books
+            )
+        return books > 0
     except Exception as e:
         logger.error(f'Failed to insert books {e}', exc_info=True)
         print(f'Failed to insert books...: {e}')
+
+
+def _delete_books(department_name: str, book_name: str):
+    is_exists = Books.objects(**{
+        f'{department_name}__exists': True,
+        f'{department_name}__match': {'Title': book_name}
+        }).update_one(
+        **{f'pull__{department_name}': {'Title': book_name}}
+        )
+    if is_exists:
+        return is_exists
+
+
+def _find_books(department_name: str, book_name: str):
+    is_exists = Books.objects(**{
+        f'{department_name}__exists': True,
+        f'{department_name}__match': {'Title': book_name}
+    })
+
+    if is_exists:
+        return is_exists
+
+
+# if __name__ == '__main__':
+    # _delete_books('bba', 'b')
+    # _find_books('bbddds')
