3052dbc89c83b33f1696c5d68a5ed9fd417f6ea8
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-14 18:25:08 +0545

    written _updated_books function

--- src/utils/mongo.py
+++ src/utils/mongo.py
@@ -1,9 +1,9 @@
 # third party modules
-from src.models import Books
+from src.models import Books, BookCategories
 from src.utils import logger
 
 
-def insert_books(department_name: str, new_books: list[int | str]):
+def _insert_books(department_name: str, new_books: list[int | str]):
     """implemented mongoengine module to add books
 
     Args:
@@ -20,13 +20,23 @@ def insert_books(department_name: str, new_books: list[int | str]):
             **{f'push_all__{department_name}': new_books},
             upsert=True
             )
-        return books > 0
+        if books:
+            return books
     except Exception as e:
         logger.error(f'Failed to insert books {e}', exc_info=True)
         print(f'Failed to insert books...: {e}')
 
 
 def _delete_books(department_name: str, book_name: str):
+    """delete books
+
+    Args:
+        department_name (str): category of book
+        book_name (str): book name
+
+    Returns:
+        int: it return 1, mean successfully deleted book
+    """
     is_exists = Books.objects(**{
         f'{department_name}__exists': True,
         f'{department_name}__match': {'Title': book_name}
@@ -38,6 +48,15 @@ def _delete_books(department_name: str, book_name: str):
 
 
 def _find_books(department_name: str, book_name: str):
+    """to search books
+
+    Args:
+        department_name (str): book category
+        book_name (str): book name
+
+    Returns:
+        int: it return 1, mean successfully find book
+    """
     is_exists = Books.objects(**{
         f'{department_name}__exists': True,
         f'{department_name}__match': {'Title': book_name}
@@ -46,6 +65,29 @@ def _find_books(department_name: str, book_name: str):
         return is_exists
 
 
-# if __name__ == '__main__':
-    # _delete_books('bba', 'b')
-    # _find_books('bbddds')
+def _update_books(
+        department_name: str, old_book_name: str, new_books: BookCategories):
+    """to update books
+
+    Args:
+        department_name (str): book category
+        old_book_name (str): previous book name
+        new_books (_type_): BookCategory Model Instance
+
+    Returns:
+        int: 1 mean successfully updated book
+    """
+    is_exists = Books.objects(**{
+        f'{department_name}__exists': True,
+        f'{department_name}__match': {'Title': old_book_name}
+    }).update_one(
+        __raw__={
+            '$set': {
+                f'{department_name}.$.Title': new_books.Title,
+                f'{department_name}.$.Author': new_books.Author,
+                f'{department_name}.$.Available': new_books.Available
+            }
+        }
+    )
+    if is_exists:
+        return is_exists
