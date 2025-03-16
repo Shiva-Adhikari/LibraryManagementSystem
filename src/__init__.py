323869620224e9cbc5bb69cc8c67b457c88ddb9b
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-16 14:01:20 +0545

    try to rewrite, http_server from scratch

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.account_manager import account_register, account_login
-
-__all__ = ['account_register', 'account_login']
--- src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .settings import settings, db
-from .account import AccountRegisterModel
-from .mongo import Books, BookCategories
-
-__all__ = [
-    'settings', 'db',
-    'AccountRegisterModel',
-    'Books', 'BookCategories'
-]
