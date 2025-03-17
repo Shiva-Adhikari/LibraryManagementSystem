cef1e913dcb0a55cdd4634ee08e9e989dada25fb
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-17 21:09:57 +0545

    added delete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from src.admin import admin_register, admin_login
+from src.admin import admin_register, admin_login, add_books, delete_books
 from src.utils import _send_response
 from src.user import user_register, user_login
 
@@ -14,6 +14,15 @@ class MainServer(BaseHTTPRequestHandler):
             user_register(self)
         elif self.path == '/api/user/login':
             user_login(self)
+        elif self.path == '/api/admin/add-books':
+            add_books(self)
+        else:
+            response = {'error': 'mistake is in path, /api/account/?'}
+            _send_response(self, response, 404)
+
+    def do_DELETE(self):
+        if self.path == '/api/admin/delete-books':
+            delete_books(self)
         else:
             response = {'error': 'mistake is in path, /api/account/?'}
             _send_response(self, response, 404)
