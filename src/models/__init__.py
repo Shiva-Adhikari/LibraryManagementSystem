6d467a5c2b28553554a7e3d984cf409f5caaef52
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-19 00:47:19 +0545

    added mongo_config

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,9 @@
-from .settings import settings, db
+from .settings import settings, db, mongo_config
 from .account import AccountRegisterModel
 from .mongo import Books, BookCategories
 
 __all__ = [
-    'settings', 'db',
+    'settings', 'db', 'mongo_config',
     'AccountRegisterModel',
     'Books', 'BookCategories'
 ]
