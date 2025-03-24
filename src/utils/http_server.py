873a93c45e56a3a47bc63d80d8d404f7d7eb95f4
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-25 04:57:15 +0545

    added access_token and refresh_token to get from header

--- src/utils/http_server.py
+++ src/utils/http_server.py
@@ -12,3 +12,30 @@ def _read_json(handler):
     content_length = int(handler.headers['Content-Length'])
     post_data = handler.rfile.read(content_length)
     return json.loads(post_data.decode('utf-8'))
+
+
+def _input_access_token(handler):
+    auth_header = handler.headers.get('Authorization')
+
+    if auth_header:
+        if auth_header.startswith('Bearer '):
+            access_token = auth_header.split(' ', 1)[1]
+        else:
+            access_token = auth_header  # Directly take the token
+
+        return access_token
+    else:
+        response = {'missing token': 'Missing or Invalid Access token'}
+        _send_response(handler, response, 401)
+        return
+
+
+def _input_refresh_token(handler):
+    auth_refresh_token = handler.headers.get('RefreshToken')
+
+    if auth_refresh_token:
+        return auth_refresh_token
+    else:
+        response = {'missing token': 'Missing or Invalid Refresh token'}
+        _send_response(handler, response, 401)
+        return
