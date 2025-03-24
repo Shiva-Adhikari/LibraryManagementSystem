203e98e0e5311c19f20934cb6580066f90ccbc51
Shiva Adhikari <s2whl6xh@duck.com>
2025-03-25 04:54:11 +0545

    removed unused functions

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,17 +1,6 @@
 from src.utils.utils_ import (
-    data_path,
     logger,
-    logout,
-    verify_jwt_token,
     find_keys,
-    tqdm_progressbar,
-    get_user_login_details,
-    get_admin_login_details,
-    remove_user_login_details,
-    remove_admin_login_details,
-    validate_access_token,
-    token_blacklist,
-    get_access_token,
     count_books,
     validate_user
 )
@@ -27,19 +16,8 @@ from src.utils.http_server import (
 
 
 __all__ = [
-    'data_path',
     'logger',
-    'logout',
-    'verify_jwt_token',
     'find_keys',
-    'tqdm_progressbar',
-    'get_user_login_details',
-    'get_admin_login_details',
-    'remove_user_login_details',
-    'remove_admin_login_details',
-    'validate_access_token',
-    'token_blacklist',
-    'get_access_token',
     'count_books',
     'validate_user',
 
