11c5665fc371882b3180f355fb1e19962bf269d5
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-01 21:49:17 +0545

    successfully converted code to mongoengine

--- src/admin/update_books.py
+++ src/admin/update_books.py
@@ -1,60 +1,45 @@
 # local modules
-from src.utils import (
-    logger, find_keys,
-    _find_books, _update_books
-)
-from src.models import BookCategories
-from src.utils import _read_json, _send_response, _verify_refresh_token
+from src.models import Department, Books_
+from src.utils import _read_json, _send_response
 
 
 def update_books(handler):
-    """Fetch and Update Books from database.
-    """
-
-    # check book list is empty or not
-    check_book = find_keys()
-    if not check_book:
-        response = {'error': 'Book Not found, Add books first'}
-        _send_response(handler, response, 500)
-        return
-
     data = _read_json(handler)
     category = data.get('category').lower().strip()
     old_book_name = data.get('old_book_name').lower().strip()
     new_book = data.get('new_book')
 
-    search_books = _find_books(category, old_book_name)
-    if search_books:
-        book_info = {}
-
-        book_info = {
-            'Title': new_book.get('title').lower().strip(),
-            'Author': new_book.get('author').lower().strip(),
-            'Available': new_book.get('available')
+    department = Department.objects(name=category).first()
+    if not department:
+        response = {
+            'status': 'error',
+            'message': 'Department not found'
         }
-
-        verify = _verify_refresh_token(handler, whoami='Admin')
-        if not verify:
-            response = {'error': 'Data is Discarded, please login first.'}
-            _send_response(handler, response, 500)
-            return
-
-        # Mongo Model
-        book = BookCategories(
-            Title=book_info['Title'],
-            Author=book_info['Author'],
-            Available=book_info['Available']
-        )
-
-        res = _update_books(category, old_book_name, book)
-        if res:
-            response = {'message': 'Updated Books Successfully'}
-            _send_response(handler, response, 200)
-        else:
-            response = {'error': 'Failed to Update'}
-            _send_response(handler, response, 500)
-            logger.debug('Failed to Update')
-
-    else:
-        response = {'error': 'Old Book Not Found'}
         _send_response(handler, response, 500)
+        return
+
+    # collect ids from department
+    book_ids = []
+    for book in department.books:
+        book_ids.append(book.id)
+
+    try:
+        book = Books_.objects.get(title=old_book_name, id__in=book_ids)
+    except Exception:
+        response = {
+            'status': 'error',
+            'message': 'Book Not Found to update'
+        }
+        _send_response(handler, response, 500)
+        return
+
+    book.title = new_book['title']
+    book.author = new_book['author']
+    book.available = new_book['available']
+
+    book.save()
+    response = {
+        'status': 'success',
+        'message': 'Book Updated Successfully'
+    }
+    _send_response(handler, response, 500)
