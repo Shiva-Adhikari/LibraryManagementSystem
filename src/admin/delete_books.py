d618820015d0643b2899dad15e5ed491194ffef2
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-02 09:24:54 +0545

    successfully converted code to mongoengine

--- src/admin/delete_books.py
+++ src/admin/delete_books.py
@@ -1,34 +1,50 @@
 # local modules
-from src.utils import find_keys, _delete_books
-from src.utils import _send_response, _read_json, _verify_refresh_token
+from src.models import Department, Books_
+from src.utils import _send_response, _read_json
 
 
 def delete_books(handler):
     """Delete Books from database.
     """
 
-    # check book is empty or not
-    check_book = find_keys()
-    if not check_book:
-        response = {'error': 'Books Not found, Add book first'}
-        _send_response(handler, response, 500)
-        return
-
     data = _read_json(handler)
-    category = data.get('category').lower().strip()
-    book_name = data.get('book_name').lower().strip()
+    category = data.get('category', '').lower().strip()
+    book_name = data.get('book_name', '').lower().strip()
 
-    verify = _verify_refresh_token(handler, whoami='Admin')
-    if not verify:
-        response = {'error': 'Data is Discarded, please login first.'}
-        _send_response(handler, response, 500)
-        return
+    department = Department.objects(name=category).first()
+    if not department:
+        response = {
+            'status': 'error',
+            'message': 'Department not found'
+        }
+        return _send_response(handler, response, 500)
 
-    # call function to delete books
-    books = _delete_books(category, book_name)
-    if books:
-        response = {'message': 'Successfully Deleted Book'}
-        _send_response(handler, response, 200)
-    else:
-        response = {'error': 'Book not Found'}
-        _send_response(handler, response, 200)
+    book_to_delete = Books_.objects.get(title=book_name)
+
+    if not book_to_delete:
+        response = {
+                'status': 'error',
+                'message': 'Book not found'
+            }
+        return _send_response(handler, response, 500)
+
+    # department ko ra, book ko id match vayo vani
+    if book_to_delete.id in [book.id for book in department.books]:
+        # delete reference id from department
+        department.books.remove(book_to_delete)
+        department.save()
+
+        # delete books from collection
+        book_to_delete.delete()
+
+        response = {
+            'status': 'success',
+            'message': 'Successfully Deleted Book'
+        }
+        return _send_response(handler, response, 200)
+
+    response = {
+                'status': 'error',
+                'message': 'Book not found in this department'
+            }
+    return _send_response(handler, response, 500)
