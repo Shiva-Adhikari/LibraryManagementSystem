cb33856d2031e7dbad4150dc0d0ead866cceb5cb
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-03 12:36:57 +0545

    successfully converted code to mongoengine

--- src/user/issue_books.py
+++ src/user/issue_books.py
@@ -3,90 +3,79 @@ from datetime import datetime, timedelta
 
 # local modules
 from src.utils import (
-    logger, find_keys, validate_user,
     _send_response, _read_json, _verify_refresh_token
 )
-from src.models import db
 
 
-def issue_books(handler) -> None:
+def issue_books(handler):
+    from src.models import Books_, UserDetails, Department
     """User issue book.
     """
 
-    # check if books is empty or not
-    check_books = find_keys()
-    if not check_books:
-        response = {'error': 'Books Not found, Library is Empty'}
-        _send_response(handler, response, 500)
+    data = _read_json(handler)
+    if not data:
         return
 
-    data = _read_json(handler)
-    category = data.get('category').lower().strip()
+    category_name = data.get('category').lower().strip()
     book_name = data.get('book_name').lower().strip()
     to_date = data.get('days')
 
-    try:
-        issue_date = datetime.now()
-        warning_to_date = to_date - 3
-        due_warning = issue_date + timedelta(days=warning_to_date)
-        due_date = issue_date + timedelta(days=to_date)
+    if not isinstance(to_date, int):
+        response = {
+            'status': 'error',
+            'message': 'enter days not string'
+        }
+        return _send_response(handler, response, 500)
 
-        user_detail = _verify_refresh_token(handler, whoami='User')
-        if not user_detail:
-            response = {'error': 'Data is Discarded, please login first.'}
-            _send_response(handler, response, 500)
-            return
+    issue_date = datetime.now()
+    warning_to_date = to_date - 3
+    due_warning = issue_date + timedelta(days=warning_to_date)
+    due_date = issue_date + timedelta(days=to_date)
 
-        username = user_detail['username']
-        email = user_detail['email']
-
-        # call validate_user from utils_
-        does_exist = validate_user(category, book_name, username)
-        if does_exist:
-            response = {'error': 'Book already Issued, unable to issue again'}
-            _send_response(handler, response, 500)
-            return
-
-        # Ensure book is available before issuing
-        book_info = db.Books.find_one(
-            {f'{category}.Title': book_name, f'{category}.Available': {"$gt": 0}},
-            {f'{category}.$': 1}
-        )
-        if not book_info:
-            response = {'error': 'Book not available or not found'}
-            _send_response(handler, response, 500)
-            return
-
-        result = db.Books.update_one(
-            {f'{category}.Title': book_name},
-            {
-                '$inc': {
-                    f'{category}.$.Available': -1
-                },
-
-                '$push': {
-                    f'{category}.$.UserDetails':
-                        {
-                            'Username': username,
-                            'Email': email,
-                            'Days': to_date,
-                            'IssueDate': issue_date,
-                            'DueWarning': due_warning,
-                            'DueDate': due_date,
-                        }
-
-                }
-            }
-        )
-
-        if result.modified_count > 0:
-            response = {'message': 'You got book.'}
-            _send_response(handler, response, 200)
-        else:
-            response = {'error': 'Unable to get book, Try Again.'}
-            _send_response(handler, response, 500)
-
-    except Exception as e:
-        logger.error(e)
-        response = {'exception': f'Unable to get book, Try Again. as {str(e)}'}
+    user_detail = _verify_refresh_token(handler, whoami='User')
+    if not user_detail:
+        response = {'error': 'Data is Discarded, please login first.'}
         _send_response(handler, response, 500)
+        return
+
+    username = user_detail['username']
+    email = user_detail['email']
+
+    department = Department.objects(name=category_name).first()
+    if not department:
+        response = {
+            'status': 'error',
+            'message': 'department not found'
+        }
+        return _send_response(handler, response, 500)
+
+    department_ids = []
+    for book in department.books:
+        department_ids.append(book.id)
+
+    books = Books_.objects(title=book_name, id__in=department_ids).first()
+
+    if not books:
+        response = {
+            'status': 'error',
+            'message': 'book not found'
+        }
+        return _send_response(handler, response, 500)
+
+    book_info = {
+        'username': username.lower(),
+        'email': email.lower(),
+        'days': to_date,
+        'issue_date': issue_date,
+        'due_warning': due_warning,
+        'due_date': due_date
+    }
+    user_details = UserDetails(**book_info)
+    user_details.save()
+    books.user_details.append(user_details.id)
+    books.save()
+    response = {
+        'status': 'success',
+        'message': 'Successfully issued book'
+    }
+    return _send_response(handler, response, 200)
