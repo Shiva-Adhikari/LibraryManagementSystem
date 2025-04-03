50f27a094e089a7a6a860bba38751486a4cb407e
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-03 23:47:21 +0545

    successfully converted code to mongoengine

--- src/user/list_books.py
+++ src/user/list_books.py
@@ -1,100 +1,56 @@
 # local modules
-from src.utils import (
-    _send_response, _read_json, _verify_refresh_token
-)
-from src.models import db
-
-
-books_keys = []
-
-
-def connect_database():
-    """Get list of books
-
-    Returns:
-        list: return books in list.
-    """
-
-    fetch_books = db.Books.find({}, {'_id': 0})
-    for fetch_keys in fetch_books:
-        books_keys.append(list(fetch_keys.keys()))
-
-    if books_keys:
-        return books_keys
-
-
-def list_view(handler, category, page_no):
-    """Display books in table view.
-
-    Args:
-        category (str): book category like (BCA, BBA, BBS)
-        page_no (int): page no.
-
-    Returns:
-        bool: if user input invalid page then return False or exit.
-    """
-
-    # list books from database
-    count_books = db.Books.aggregate([
-        {'$unwind': f'${category}'},
-        {'$count': 'total'}
-    ]).next()['total']
-
-    page_size = 5  # Number of elements per page
-    total_pages = (count_books + page_size - 1) // page_size
-
-    if page_no < 1 or page_no > total_pages:
-        response = {'invalid': f'Invalid page, Available pages up to {total_pages}'}
-        _send_response(handler, response, 500)
-        return
-
-    skip_line = (page_no - 1) * page_size     # Calculate skip value
-    pipeline = [
-        {'$match': {category: {'$exists': True}}},
-        {'$unwind': f'${category}'},
-        {'$skip': skip_line},
-        {'$limit': page_size}
-    ]
-
-    find_books_page_1 = list(db.Books.aggregate(pipeline))
-
-    table = []
-    for extract in find_books_page_1:
-        table.append({
-            'Id': extract[category]['Id'],
-            'Book Name': extract[category]['Title'].capitalize(),
-            'Author': extract[category]['Author'].capitalize(),
-            'Available': 'Yes' if extract[category]['Available'] else 'No'
-        })
-
-    response = {
-        'Book List': table,
-        'page': f'Page {page_no} of {total_pages}'
-    }
-    _send_response(handler, response, 200)
-
-    global books_keys
-    books_keys = []
+from src.utils import _read_json, _send_response
+from src.models import Department, Books_
 
 
 def list_books(handler):
     """Display list of Books.
     """
 
-    books_keys = connect_database()
-    if not books_keys:
-        response = {'error': 'Books Not found, Library is Empty'}
-        _send_response(handler, response, 500)
-        return
-
     data = _read_json(handler)
+    if not data:
+        return
     category = data.get('category').lower()
     page_no = data.get('page')
 
-    user_detail = _verify_refresh_token(handler, whoami='User')
-    if not user_detail:
-        response = {'error': 'Data is Discarded, please login first.'}
+    department = Department.objects(name=category).first()
+    if not department:
+        print('department not found')
+        return
+
+    department_ids = []
+    for book in department.books:
+        department_ids.append(book.id)
+
+    page_size = 5
+    total_books = len(department.books)
+    total_pages = (total_books + page_size - 1)
+
+    if page_no < 1 or page_no > total_pages:
+        response = {'invalid': f'Invalid page, Available pages up to {total_pages}'}
         _send_response(handler, response, 500)
         return
 
-    list_view(handler, category, page_no)
+    skip_line = (page_no - 1) * page_size
+
+    books = Books_.objects(id__in=department_ids).skip(skip_line).limit(page_size)
+
+    # convert to readable JSON-like format
+    books_list = [book.to_mongo().to_dict() for book in books]
+    # print(f'booklist: {books_list}')
+
+    list_books = [
+        {
+            'category': category,
+            'title': book['title'],
+            'author': book['author'],
+            'available': book['available'],
+        }
+        for book in books_list
+    ]
+
+    response = {
+        'status': 'success',
+        'message': list_books
+    }
+    return _send_response(handler, response, 200)
