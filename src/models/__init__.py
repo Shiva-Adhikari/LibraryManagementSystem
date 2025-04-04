f39789b28d07a7bc398b89dd97bb84ca3c0e91d6
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-04 15:38:33 +0545

    removed unused functions

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,12 @@
 from .settings import settings, db, mongo_config, http_server
-from .account import AccountRegisterModel
+from .account import (
+    Account, AccountDetails
+)
 from .mongo import Department, Books, UserDetails
 
 __all__ = [
     'settings', 'db', 'mongo_config',
-    'AccountRegisterModel',
+    'Account', 'AccountDetails',
     'http_server',
     'Department', 'Books', 'UserDetails'
 ]
