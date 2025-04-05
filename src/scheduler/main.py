3a7371c6f6344155bdffa42c420e8353751baa3a
Shiva Adhikari <s2whl6xh@duck.com>
2025-04-05 10:43:49 +0545

    successfully converted code to mongoengine

--- src/scheduler/main.py
+++ src/scheduler/main.py
@@ -1,58 +1,38 @@
 # third party modules
-from apscheduler.schedulers.blocking import BlockingScheduler  # type: ignore
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # built in modules
 from datetime import datetime
 
 # local modules
-from src.utils import find_keys
-from src.models import db
+from src.models import UserDetails
 from .send_email import send_email
 
 
 def due_book_check():
     """if date is 3 days left or due date it send email to users in mailbox.
     """
-    category_key = find_keys()
-    today_date_ = datetime.now()
-    today_date = datetime.date(today_date_)
-    for category in category_key:
-        fetch_data = db.Books.aggregate([
-            {'$unwind': f'${category}'},
-            {'$unwind': f'${category}.UserDetails'},
-            {
-                '$match': {
-                        f'{category}.UserDetails.IssueDate': {
-                            '$lte': today_date_
-                        }
-                }
-            },
-            {
-                '$project': {
-                    'Username': f'${category}.UserDetails.Username',
-                    'Email': f'${category}.UserDetails.Email',
-                    # 'DueWarning': f'${category}.UserDetails.IssueDate',   # for testing purpose
-                    'DueWarning': f'${category}.UserDetails.DueWarning',
-                    'DueDate': f'${category}.UserDetails.DueDate'
-                }
-            }
-        ])
-        for book in fetch_data:
-            due_warning = book['DueWarning']
-            due_warning_day = datetime.date(due_warning)
-            due_date = book['DueDate']
-            due_date_day = datetime.date(due_date)
+    _today_date = datetime.now()
+    today_date = datetime.date(_today_date)
+    user_details = UserDetails.objects()
 
-            if today_date == due_warning_day:
-                user_username = book['Username']
-                user_email = book['Email']
-                due_warning_text = 'DueWarning'
-                send_email(user_username, user_email, due_warning_text)
-            if today_date == due_date_day:
-                user_username = book['Username']
-                user_email = book['Email']
-                due_date_text = 'DueDate'
-                send_email(user_username, user_email, due_date_text)
+    for user in user_details:
+        due_warning = user.due_warning
+        due_warning_day = datetime.date(due_warning)
+        due_date = user.due_date
+        due_date_day = datetime.date(due_date)
+
+        if today_date == due_warning_day:
+            user_username = user.username
+            user_email = user.email
+            due_warning_text = 'DueWarning'
+            send_email(user_username, user_email, due_warning_text)
+
+        if today_date == due_date_day:
+            user_username = user.username
+            user_email = user.email
+            due_date_text = 'DueDate'
+            send_email(user_username, user_email, due_date_text)
 
 
 if __name__ == '__main__':
